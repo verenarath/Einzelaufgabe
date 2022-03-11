bcbe2a810a441a6190e07bea81a610c02345bcb8
Verena Rath <verenarath@MacBook-Pro-von-Verena.local>
2022-03-11 22:07:35 +0100

    new button and changed id names

--- app/src/main/java/com/example/einzelaufgabe/MainActivity.java
+++ app/src/main/java/com/example/einzelaufgabe/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.einzelaufgabe;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
